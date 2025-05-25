6a9fa705e5254f0445bd2575b0d4eb4b988e8e1c
lakshmi <nagalakshmireddy67@gmail..com>
2025-05-25 10:34:44 +0530

    This is 11th commit

--- special-vars.sh
+++ special-vars.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "All variables passed to the scrpit : $@"
+echo "Number of varaibles : $# "
+echo "scrpit name :$0"
+echo "current directry : $PWD"
+echo "User running the scrpit : $USER"
+echo "Home directory of the scrpit: $HOME "
+echo "PID of the scrpit: $$ "
+sleep 10
+echo "PID of last command in background: $!"
