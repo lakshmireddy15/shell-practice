2d58f8a0236375d31624dded66255cb1411a1a54
lakshmi <nagalakshmireddy67@gmail..com>
2025-05-25 11:38:53 +0530

    This is 19th commit

--- install-command.sh
+++ install-command.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Error:: Run this scrpit with root acess"
+    exit 1 # give other than 0 upto 127
 else
     echo " You are running with root acess "
 fi
