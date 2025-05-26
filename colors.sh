2255a29a916182065dd0ed7f226c29fa389d5713
lakshmi <nagalakshmireddy67@gmail..com>
2025-05-26 09:27:51 +0530

    This is 20th commit

--- colors.sh
+++ colors.sh
@@ -1,4 +1,10 @@
 !#/bin/bash
 
-echo -e "\e[31m Hello colours "
+# It will give colors to the 4th and 5th line beacuse we are not closing the 
+#colors
+# echo -e "\e[31m Hello colours "
+# echo -e "No colours"
+
+
+echo -e "\e[31m Hello colours \e[0m"
 echo -e "No colours"
